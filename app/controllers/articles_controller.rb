fdc50c3a1debc23c36ffd205dd2ed978401ec566
Stephanyan <rabena.stephane@gmail.com>
2015-04-01 20:45:39 +0200

    Setup imagemagick

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -46,7 +46,7 @@ class ArticlesController < ApplicationController
   private
 
   def articles_params
-    params.require(:article).permit(:title, :summary, :description, :author)
+    params.require(:article).permit(:title, :summary, :description, :author, :image)
   end
 
   # def find_article
