96d5761e2ab1050df9b7e1e3c01b01a33c2fa712
Stephanyan <rabena.stephane@gmail.com>
2015-04-03 18:19:35 +0200

    set default size in cards

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
   # TODO before_action :find_article, only: [:show, :edit, :update, :destroy]
   def index
-    @articles = Article.all.order("created_at DESC").paginate(page: params[:page], per_page: 10)
+    @articles = Article.all.order("created_at DESC").paginate(page: params[:page], per_page: 9)
   end
 
   def show
